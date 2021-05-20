d2cb9d136d676f463e301099d6635a88e1363ab2
unknown <kristjantryggva@hotmail.com>
2021-05-20 15:41:38 +0000

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -3,9 +3,13 @@ const core = require("@actions/core");
 const firstGreeting = core.getInput("first-greeting");
 const secondGreeting = core.getInput("second-greeting");
 const thirdGreeting = core.getInput("third-greeting");
+const booleantest = core.getInput("boolean-test");
 
 console.log(`Hello ${firstGreeting}`);
 console.log(`Hello ${secondGreeting}`);
 if (thirdGreeting) {
   console.log(`Hello ${thirdGreeting}`);
+}
+if (booleantest) {
+  console.log(`you did not input false`);
 }
